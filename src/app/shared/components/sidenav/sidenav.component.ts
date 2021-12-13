80ed87be5e1198186c4cd0c2b8c0fb2f232c29b4
Pratik Shah <psh@narola.email>
2021-12-13 14:43:26 +0530

    Sidenav design changes

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -222,6 +222,28 @@ export class SidenavComponent implements OnInit {
   clearModal():void {
     this.featureDescription = '';
   }
+
+  toggleClicked(event) {
+    var target = event.srcElement.id;
+    var body = $("body");
+    var menu = $("#sidebar-menu");
+
+    // toggle small or large menu
+    if (body.hasClass("nav-md")) {
+      menu.find("li.active ul").hide();
+      menu
+        .find("li.active")
+        .addClass("active-sm")
+        .removeClass("active");
+    } else {
+      menu.find("li.active-sm ul").show();
+      menu
+        .find("li.active-sm")
+        .addClass("active")
+        .removeClass("active-sm");
+    }
+    body.toggleClass("nav-md nav-sm");
+  }
 }
 
 export interface FeatureType {
