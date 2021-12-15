2020d841aeb286476edcc8be9a9b3fe26ea252d2
Pratik Shah <psh@narola.email>
2021-12-15 15:07:20 +0530

    add active class on meeting preview table tab

--- src/app/modules/meeting/meeting-preview/meeting-preview.component.ts
+++ src/app/modules/meeting/meeting-preview/meeting-preview.component.ts
@@ -11,101 +11,108 @@ import { IssueDataService } from '../../issue/data.service'
   styleUrls: ['./meeting-preview.component.scss']
 })
 
-export class MeetingPreviewComponent implements OnInit,OnDestroy {
+export class MeetingPreviewComponent implements OnInit, OnDestroy {
 
-  @Input() previewMeetingId:string;
-  @Input() userRole:string;
+  @Input() previewMeetingId: string;
+  @Input() userRole: string;
   @Output() launchMeetingId = new EventEmitter<any>();
 
-  constructor(private MeetingDataService: MeetingDataService, private IssueDataService: IssueDataService ) { }
+  constructor(private MeetingDataService: MeetingDataService, private IssueDataService: IssueDataService) { }
 
-  dataSubscription:Subscription[] = [];
+  dataSubscription: Subscription[] = [];
 
-  meetingData:any;
-  issueData:Array<boolean> = [];
-  milestoneData:Array<boolean> = [];
-  issueDataFiltered:Array<boolean> = [];
-  milestoneDataFiltered:Array<boolean> = [];
+  meetingData: any;
+  issueData: Array<boolean> = [];
+  milestoneData: Array<boolean> = [];
+  issueDataFiltered: Array<boolean> = [];
+  milestoneDataFiltered: Array<boolean> = [];
 
   ngOnInit() {
     this.setSubscriptions();
+    // this.getKpiData(this.previewMeetingId);
   }
 
-  setSubscriptions():void {
-    this.dataSubscription.push(this.MeetingDataService.getUserMeetingDataDetailData.subscribe((data:any)=>{
+  setSubscriptions(): void {
+    this.dataSubscription.push(this.MeetingDataService.getUserMeetingDataDetailData.subscribe((data: any) => {
       this.meetingData = data.payload[0];
+      this.meetingData.displayKPI = true;
       this.meetingData.displayIssues = false;
       this.meetingData.displayMilestones = false;
       this.meetingData.selectedUser = null;
     }))
 
-    setTimeout(() => {
-      console.log(': ===> userRole', this.meetingData);
-    }, 500);
     this.MeetingDataService.getUserMeetingDataDetail(this.previewMeetingId);
 
-    this.dataSubscription.push(this.IssueDataService.getMeetingIssueData.subscribe((data:any)=>{
+    this.dataSubscription.push(this.IssueDataService.getMeetingIssueData.subscribe((data: any) => {
       this.issueData = data;
     }));
 
-    this.dataSubscription.push(this.IssueDataService.getUserIssueData.subscribe((data:any)=>{
-        this.issueDataFiltered = data;
+    this.dataSubscription.push(this.IssueDataService.getUserIssueData.subscribe((data: any) => {
+      this.issueDataFiltered = data;
     }));
 
-    this.dataSubscription.push(this.IssueDataService.getMeetingMilestoneData.subscribe((data:any)=>{
+    this.dataSubscription.push(this.IssueDataService.getMeetingMilestoneData.subscribe((data: any) => {
       this.milestoneData = data;
     }));
   }
 
-  ngOnDestroy():void {
+  ngOnDestroy(): void {
     this.destroySubscriptions();
   }
 
-  destroySubscriptions():void {
-    this.dataSubscription.forEach(s=>s.unsubscribe());
+  destroySubscriptions(): void {
+    this.dataSubscription.forEach(s => s.unsubscribe());
   }
 
-  getIssueData(meetingId:string):void {
-    this.meetingData.displayIssues = !this.meetingData.displayIssues;
+  getIssueData(meetingId: string): void {
+    this.meetingData.displayIssues = true;
     this.meetingData.displayMilestones = false;
+    this.meetingData.displayKPI = false;
     this.IssueDataService.getMeetingIssue(meetingId);
   }
 
-  getMilestoneData(meetingID:string):void {
-    this.meetingData.displayMilestones = !this.meetingData.displayMilestones;
+  getMilestoneData(meetingID: string): void {
+    this.meetingData.displayMilestones = true;
     this.meetingData.displayIssues = false;
+    this.meetingData.displayKPI = false;
     this.IssueDataService.getMeetingMilestone(meetingID);
   }
 
-  setFilter(type:string):void {
-    if(type=='issues'){
+  getKpiData(meetingID: string): void {
+    this.meetingData.displayKPI = true;
+    this.meetingData.displayMilestones = false;
+    this.meetingData.displayIssues = false;
+  }
+
+  setFilter(type: string): void {
+    if (type == 'issues') {
       this.meetingData.selectedUser.filterIssues = !this.meetingData.selectedUser.filterIssues
-    }else if (type=='milestones'){
+    } else if (type == 'milestones') {
       this.meetingData.selectedUser.filterMilestones = !this.meetingData.selectedUser.filterMilestones
     }
   }
 
-  getUserIssue():void {
-    if(this.meetingData.selectedUser.filterIssues){
+  getUserIssue(): void {
+    if (this.meetingData.selectedUser.filterIssues) {
       this.IssueDataService.getMeetingUserIssue(this.meetingData.selectedUser.uID);
     } else {
       this.IssueDataService.getMeetingIssue(this.meetingData.meetingId);
     }
   }
 
-  getUserMilestone():void {
-    if(this.meetingData.selectedUser.filterMilestones){
-      this.milestoneDataFiltered = this.milestoneData.filter((user:any)=>user.uID===this.meetingData.selectedUser.uID)
+  getUserMilestone(): void {
+    if (this.meetingData.selectedUser.filterMilestones) {
+      this.milestoneDataFiltered = this.milestoneData.filter((user: any) => user.uID === this.meetingData.selectedUser.uID)
     } else {
       this.milestoneDataFiltered = [] // does this cause issues?
       this.IssueDataService.getMeetingMilestone(this.meetingData.meetingId);
     }
   }
 
-  setSelectedUser(user:Object):void {
-    for(const u in this.meetingData.meetingUsers){
-      if(user['uID']==this.meetingData.meetingUsers[u].uID){
-        if(this.meetingData.selectedUser && user['uID']==this.meetingData.selectedUser.uID){
+  setSelectedUser(user: Object): void {
+    for (const u in this.meetingData.meetingUsers) {
+      if (user['uID'] == this.meetingData.meetingUsers[u].uID) {
+        if (this.meetingData.selectedUser && user['uID'] == this.meetingData.selectedUser.uID) {
           this.meetingData.meetingUsers[u].selected = false
         } else {
           this.meetingData.meetingUsers[u].selected = true
@@ -123,19 +130,17 @@ export class MeetingPreviewComponent implements OnInit,OnDestroy {
     this.getUserMilestone();
   }
 
-  launchMeeting(meetingId:any){
-    console.log(': ===> meeting', meetingId);
-
-    var obj = { 
-                attendees: null,
-                meeting: this.meetingData.meetingName,
-                meetingDate: this.meetingData.meetingDate,
-                meetingFrequency: this.meetingData.meetingFrequency,
-                meetingId: meetingId,
-                meetingInterval: this.meetingData.meetingInterval,
-                timerData: this.meetingData.timerData,
-                userRole: this.userRole
-              }
+  launchMeeting(meetingId: any) {
+    var obj = {
+      attendees: null,
+      meeting: this.meetingData.meetingName,
+      meetingDate: this.meetingData.meetingDate,
+      meetingFrequency: this.meetingData.meetingFrequency,
+      meetingId: meetingId,
+      meetingInterval: this.meetingData.meetingInterval,
+      timerData: this.meetingData.timerData,
+      userRole: this.userRole
+    }
     this.launchMeetingId.emit(obj)
   }
 }
